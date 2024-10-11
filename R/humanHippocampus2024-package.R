13bc9274752a4d0bfed158c0db994c64b8379dc9
christinehou11 <chou11@jh.edu>
2024-10-11 11:23:34 -0400

    new

--- R/humanHippocampus2024-package.R
+++ R/humanHippocampus2024-package.R
@@ -7,7 +7,8 @@
 #'
 #' @description This is an ExperimentHub package that provides access to the
 #' data generated and analyzed in the
-#' [humanHippocampus2024](https://github.com/LieberInstitute/spatial_hpc) project.
+#' [humanHippocampus2024](https://github.com/LieberInstitute/spatial_hpc)
+#' project.
 #' License: Artistic-2.0
 #' @name humanHippocampus2024-package
 #' @aliases humanHippocampus2024-package human-hippocampus
