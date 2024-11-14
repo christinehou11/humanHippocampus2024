03e58e73dce16e875532a10af7d8b421efb50bf1
christinehou11 <chou11@jh.edu>
2024-11-14 15:42:51 -0500

    version bump up

--- R/humanHippocampus2024-package.R
+++ R/humanHippocampus2024-package.R
@@ -1,17 +1,20 @@
-#' @keywords internal
-#'
-"_PACKAGE"
+## Note: The @importFrom lines exist to silence an R CMD check warning.
+## These packages are required to load the datasets, but are not used directly.
 
-## usethis namespace: start
-#' Get access to the spatial HPC project data
-#'
+#' @keywords internal
+#' @title Get access to the spatial HPC project data
 #' @description This is an ExperimentHub package that provides access to the
 #' data generated and analyzed in the
 #' [humanHippocampus2024](https://github.com/LieberInstitute/spatial_hpc)
 #' project.
-#' License: Artistic-2.0
+#' @docType package
+#' @importFrom SummarizedExperiment SummarizedExperiment
+#' @importFrom SpatialExperiment SpatialExperiment
 #' @name humanHippocampus2024-package
 #' @aliases humanHippocampus2024-package human-hippocampus
 #' @author Christine Hou
+"_PACKAGE"
+
+## usethis namespace: start
 ## usethis namespace: end
 NULL
